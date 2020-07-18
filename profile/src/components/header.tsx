8f894138c024468226c28518ce341b49f1710501
AkiraFukushima <h3.poteto@gmail.com>
2020-07-18 22:43:12 +0900

    Create header

--- profile/src/components/header.tsx
+++ profile/src/components/header.tsx
@@ -1,34 +1,73 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Div, Text } from "atomize"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+  <Div
+    bg="success900"
+    d="flex"
+    tag="header"
+    justify="space-between"
+    textColor="gray400"
+    p={{ x: "10rem", y: "1rem" }}
+    m={{ b: "1rem" }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+    <Div>
+      <Link
+        to="/"
+        style={{
+          textDecoration: `none`,
+        }}
+      >
+        <Text tag="h1" textSize="title" textColor="gray400">
           {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+        </Text>
+      </Link>
+    </Div>
+    <Div d="flex" justify="flex-end">
+      <Text
+        tag="h2"
+        textSize="subheader"
+        textColor="gray400"
+        m={{ x: "0.5rem", y: 0 }}
+      >
+        About Me
+      </Text>
+      <Text
+        tag="h2"
+        textSize="subheader"
+        textColor="gray400"
+        m={{ x: "0.5rem", y: 0 }}
+      >
+        Career
+      </Text>
+      <Text
+        tag="h2"
+        textSize="subheader"
+        textColor="gray400"
+        m={{ x: "0.5rem", y: 0 }}
+      >
+        OSS
+      </Text>
+      <Text
+        tag="h2"
+        textSize="subheader"
+        textColor="gray400"
+        m={{ x: "0.5rem", y: 0 }}
+      >
+        Talks
+      </Text>
+      <Text
+        tag="h2"
+        textSize="subheader"
+        textColor="gray400"
+        m={{ x: "0.5rem", y: 0 }}
+      >
+        Skills
+      </Text>
+    </Div>
+  </Div>
 )
 
 Header.propTypes = {
--- profile/src/components/layout.tsx
+++ profile/src/components/layout.tsx
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { StyleReset } from "atomize"
 
 import Header from "./header"
 import "./layout.css"
@@ -25,6 +26,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <StyleReset />
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
