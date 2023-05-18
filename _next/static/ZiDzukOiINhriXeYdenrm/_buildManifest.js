c5dfdd41dcfc3c49076829d0943f279b3b2db05f
h3poteto <h3poteto@users.noreply.github.com>
2023-05-18 12:52:37 +0000

    deploy: 25e996862bcdede401c1fbfabe7046512da583eb

---
+++ _next/static/ZiDzukOiINhriXeYdenrm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/704-81d35a8a0b6ddc6b.js","static/css/5ae11f365dbec340.css","static/chunks/pages/index-ea122476ee8399f4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
