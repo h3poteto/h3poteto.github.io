1e70cb7ed7ea6586c38ae12d34cdd3c25f11afb7
h3poteto <h3poteto@users.noreply.github.com>
2024-06-12 15:47:41 +0000

    deploy: 0fec0c2c6154d57534df635d472e147784b5ff29

---
+++ _next/static/DArIYwplr6D6AUfB3GQcg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/101-0898edcef3b10c8e.js","static/css/ac58b81597a44a79.css","static/chunks/pages/index-a9abab364f1f2859.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
